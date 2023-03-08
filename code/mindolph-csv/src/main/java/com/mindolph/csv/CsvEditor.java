c8c2dd1ce0693b699e65eb899b1a79e07bc4f9b4
mindolph <mindolph.com@gmail.com>
2023-03-08 16:10:27 +0800

    using TextFieldTableCell

--- code/mindolph-csv/src/main/java/com/mindolph/csv/CsvEditor.java
+++ code/mindolph-csv/src/main/java/com/mindolph/csv/CsvEditor.java
@@ -5,13 +5,17 @@ import com.mindolph.base.editor.BaseEditor;
 import com.mindolph.core.constant.SupportFileTypes;
 import com.mindolph.core.search.TextSearchOptions;
 import javafx.application.Platform;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.input.KeyEvent;
+import javafx.util.converter.DefaultStringConverter;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -56,12 +60,13 @@ public class CsvEditor extends BaseEditor implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         log.info("Initialize");
+        tableView.setPlaceholder(new Label("No content of this CSV file"));
         tableView.setEditable(true);
         tableView.setFocusTraversable(true);
-        tableView.focusedProperty().addListener((observable, oldValue, newValue) -> log.debug("Focused?" + newValue));
-        tableView.focusModelProperty().addListener((observable, oldValue, newValue) -> {
-            log.debug("Focus is changed to: %s".formatted(newValue.getFocusedCell()));
-        });
+//        tableView.focusedProperty().addListener((observable, oldValue, newValue) -> log.debug("Focused?" + newValue));
+//        tableView.focusModelProperty().addListener((observable, oldValue, newValue) -> {
+//            log.debug("Focus is changed to: %s".formatted(newValue.getFocusedCell()));
+//        });
     }
 
     @Override
@@ -101,7 +106,7 @@ public class CsvEditor extends BaseEditor implements Initializable {
 
             this.saveToCache();
 
-            Platform.runLater(() -> tableView.edit(activeRow, activeColumn));
+//            Platform.runLater(() -> tableView.edit(activeRow, activeColumn));
 
             afterLoading.run();
             this.editorReadyEventHandler.onEditorReady();
@@ -116,62 +121,132 @@ public class CsvEditor extends BaseEditor implements Initializable {
 
     private void appendColumn(String header) {
         log.debug("Append new column '%s'".formatted(header));
-        TableColumn<Row, TextField> column = new TableColumn<>(header);
+        TableColumn<Row, String> column = new TableColumn<>(header);
         column.setMinWidth(80);
         column.setEditable(true);
-//        column.setCellFactory(new Callback<TableColumn<Row, TextField>, TableCell<Row, TextField>>() {
-//            @Override
-//            public TableCell<Row, TextField> call(TableColumn<Row, TextField> param) {
-//                return null;
-//            }
-//        });
+        column.setOnEditCancel(event -> {
+            log.debug("setOnEditCancel");
+        });
+        column.setOnEditCommit(event -> {
+            log.debug("setOnEditCommit");
+        });
+        column.setOnEditStart(event -> {
+            log.debug("setOnEditStart");
+        });
+        column.setCellFactory(param -> {
+            TextFieldTableCell<Row, String> textCell = new TextFieldTableCell<>(){
+
+            };
+            textCell.setEditable(true);
+            textCell.setConverter(new DefaultStringConverter());
+            textCell.setOnKeyTyped(new EventHandler<KeyEvent>() {
+                @Override
+                public void handle(KeyEvent event) {
+                    // ignore
+                }
+            });
+            // raise after editing committed.
+            textCell.textProperty().addListener((observable, oldValue, newValue) -> {
+                if (!StringUtils.equals(oldValue, newValue) && StringUtils.isNotBlank(newValue)) {
+                    log.debug("Text changed from '%s' to '%s'".formatted(oldValue, newValue));
+                    int colIdx = tableView.getColumns().indexOf(textCell.getTableColumn());
+                    int dataIdx = colIdx - 1;// -1 because of the index column.
+//                    row.updateValue(dataIdx, newValue);
+                    Platform.runLater(() -> {
+
+                        int rowIdx = textCell.getIndex();
+                        if (rowIdx == 0) {
+                            textCell.getTableColumn().setText(newValue); // for all columns
+                        }
+                        else if (rowIdx == stubRowIdx - 1) {
+                            appendStubRow(); // last row editing activates new row.
+                        }
+                        if (colIdx == stubColIdx - 1) {
+                            appendColumn(""); // append a stub column
+                            Platform.runLater(() -> {
+                                if (activeInput == null) {
+                                    log.debug("No active input to focus");
+                                }
+                                else {
+                                    log.debug("Focus the active input at: %d %s".formatted(activeRow, activeColumn));
+                                    tableView.edit(activeRow, activeColumn);
+                                }
+                            });
+                        }
+                        fileChangedEventHandler.onFileChanged(editorContext.getFileData());
+                    });
+                }
+            });
+            textCell.focusedProperty().addListener((observable1, oldValue1, isFocused) -> {
+                if (!isFocused) {
+                    log.debug("Commit edit");
+                    textCell.commitEdit(textCell.getText());
+                    int colIdx = tableView.getColumns().indexOf(textCell.getTableColumn());
+                    Row row = tableView.getItems().get(textCell.getIndex());
+                    row.updateValue(colIdx, textCell.getText());
+                }
+            });
+            textCell.editingProperty().addListener((observable, oldValue, isEditing) -> {
+                log.debug("editing: " + isEditing);
+                if (!isEditing) {
+                    log.debug("Commit edit: " + StringUtils.abbreviate(textCell.getText(), 50));
+                    textCell.commitEdit(textCell.getText());
+                    int colIdx = tableView.getColumns().indexOf(textCell.getTableColumn());
+                    Row row = tableView.getItems().get(textCell.getIndex());
+                    row.updateValue(colIdx, textCell.getText());
+                }
+            });
+
+            return textCell;
+        });
         column.setCellValueFactory(cellData -> {
             TextField textField = new TextField();
             Row row = cellData.getValue();
             int colIdx = cellData.getTableView().getColumns().indexOf(cellData.getTableColumn());
             int dataIdx = colIdx - 1;// -1 because of the index column.
             if (dataIdx >= 0 && dataIdx < row.size()) {
-                textField.setText(row.getData().get(dataIdx));
+//                textField.setText(row.getData().get(dataIdx));
+                return new SimpleStringProperty(row.getData().get(dataIdx));
             }
-            textField.textProperty().addListener((observable, oldValue, newValue) -> {
-                if (!StringUtils.equals(oldValue, newValue) && StringUtils.isNotBlank(newValue)) {
-                    row.updateValue(dataIdx, newValue);
-                    if (row.getIndex() == 0) {
-                        cellData.getTableColumn().setText(newValue); // for all columns
-                    }
-                    else if (row.getIndex() == stubRowIdx - 1) {
-                        appendStubRow(); // last row editing activates new row.
-                    }
-                    if (colIdx == stubColIdx - 1) {
-                        appendColumn(""); // append a stub column
-                        Platform.runLater(() -> {
-                            if (activeInput == null) {
-                                log.debug("No active input to focus");
-                            }
-                            else {
-                                log.debug("Focus the active input at: %d %s".formatted(activeRow, activeColumn));
+//            textField.textProperty().addListener((observable, oldValue, newValue) -> {
+//                if (!StringUtils.equals(oldValue, newValue) && StringUtils.isNotBlank(newValue)) {
+//                    row.updateValue(dataIdx, newValue);
+//                    if (row.getIndex() == 0) {
+//                        cellData.getTableColumn().setText(newValue); // for all columns
+//                    }
+//                    else if (row.getIndex() == stubRowIdx - 1) {
+//                        appendStubRow(); // last row editing activates new row.
+//                    }
+//                    if (colIdx == stubColIdx - 1) {
+//                        appendColumn(""); // append a stub column
+//                        Platform.runLater(() -> {
+//                            if (activeInput == null) {
+//                                log.debug("No active input to focus");
+//                            }
+//                            else {
+//                                log.debug("Focus the active input at: %d %s".formatted(activeRow, activeColumn));
+////                                activeInput.requestFocus();
+//
+//                                tableView.requestFocus();
+//                                tableView.getSelectionModel().select(row);
+//                                tableView.getFocusModel().focus(activeRow, activeColumn);
 //                                activeInput.requestFocus();
-
-                                tableView.requestFocus();
-                                tableView.getSelectionModel().select(row);
-                                tableView.getFocusModel().focus(activeRow, activeColumn);
-                                activeInput.requestFocus();
-                                tableView.edit(activeRow, activeColumn);
-//                                tableView.getFocusModel().focus(1);
-                            }
-                        });
-                    }
-                    fileChangedEventHandler.onFileChanged(editorContext.getFileData());
-                }
-            });
-            textField.focusedProperty().addListener((observable, oldValue, focused) -> {
-                if (focused) {
-                    activeInput = textField;
-                    activeRow = row.getIndex();
-                    activeColumn = cellData.getTableColumn();
-                }
-            });
-            return new SimpleObjectProperty<>(textField);
+//                                tableView.edit(activeRow, activeColumn);
+////                                tableView.getFocusModel().focus(1);
+//                            }
+//                        });
+//                    }
+//                    fileChangedEventHandler.onFileChanged(editorContext.getFileData());
+//                }
+//            });
+//            textField.focusedProperty().addListener((observable, oldValue, focused) -> {
+//                if (focused) {
+//                    activeInput = textField;
+//                    activeRow = row.getIndex();
+//                    activeColumn = cellData.getTableColumn();
+//                }
+//            });
+            return null;
 //            return new TextFieldTableCell<>();
         });
         tableView.getColumns().add(column);
