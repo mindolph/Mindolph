d3a4f2d47fd9f4160cff554c5d76bb31ad0c153d
mindolph <mindolph.com@gmail.com>
2023-09-14 00:14:43 +0800

    add data migrator to production launcher

--- code/mindolph-desktop/src/main/java/com/mindolph/desktop/Launcher.java
+++ code/mindolph-desktop/src/main/java/com/mindolph/desktop/Launcher.java
@@ -2,6 +2,7 @@ package com.mindolph.desktop;
 
 import com.mindolph.base.Env;
 import com.mindolph.fx.Main;
+import com.mindolph.fx.data.DataMigrator;
 import com.mindolph.mfx.preference.FxPreferences;
 import javafx.application.Application;
 
@@ -13,6 +14,8 @@ public class Launcher {
     public static void main(String[] args) {
         Env.isDevelopment = false;
         FxPreferences.getInstance().init(Launcher.class);
+        DataMigrator dataMigrator = new DataMigrator();
+        dataMigrator.fixData();
         Application.launch(Main.class, args);
     }
 }
