b8b7b9500cc914cb8d5e729b7a4b31dcdc326fc0
mindolph <mindolph.com@gmail.com>
2023-12-04 00:33:38 +0800

    fix

--- code/mindolph-fx/src/main/java/com/mindolph/fx/view/WorkspaceView2.java
+++ code/mindolph-fx/src/main/java/com/mindolph/fx/view/WorkspaceView2.java
@@ -55,7 +55,6 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.swiftboot.util.PathUtils;
@@ -271,7 +270,7 @@ public class WorkspaceView2 extends BaseView implements EventHandler<ActionEvent
 
         btnNew.setGraphic(FontIconManager.getIns().getIcon(IconKey.PLUS));
         btnReload.setGraphic(FontIconManager.getIns().getIcon(IconKey.REFRESH));
-        btnCollapseAll.setGraphic(FontIconManager.getIns().getIcon(SystemUtils.IS_OS_MAC ? IconKey.COLLAPSE_FOLDERS : IconKey.COLLAPSE_ALL));
+        btnCollapseAll.setGraphic(FontIconManager.getIns().getIcon(IconKey.COLLAPSE_FOLDERS));
         btnFindInFiles.setGraphic(FontIconManager.getIns().getIcon(IconKey.SEARCH));
 
         // event handler for toolbar buttons.
@@ -646,7 +645,7 @@ public class WorkspaceView2 extends BaseView implements EventHandler<ActionEvent
                 contextMenu.getItems().add(miOpenInSystem);
             }
             if (isFolder) {
-                miCollapseAll = new MenuItem("Collapse All", FontIconManager.getIns().getIcon(SystemUtils.IS_OS_MAC ? IconKey.COLLAPSE_FOLDERS : IconKey.COLLAPSE_ALL));
+                miCollapseAll = new MenuItem("Collapse All", FontIconManager.getIns().getIcon(IconKey.COLLAPSE_FOLDERS));
                 miCollapseAll.setOnAction(this);
                 miFindFiles = new MenuItem("Find in Files", FontIconManager.getIns().getIcon(IconKey.SEARCH));
                 miFindFiles.setOnAction(this);
