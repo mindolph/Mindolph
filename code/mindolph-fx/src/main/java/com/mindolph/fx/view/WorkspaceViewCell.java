45917af912ce87fa1f8fb59fed2cf44526ce24d8
mindolph <mindolph.com@gmail.com>
2024-04-27 12:49:06 +0800

    feature: support dragging external files to any folder in workspace to copy those files;

--- code/mindolph-fx/src/main/java/com/mindolph/fx/view/WorkspaceViewCell.java
+++ code/mindolph-fx/src/main/java/com/mindolph/fx/view/WorkspaceViewCell.java
@@ -3,6 +3,8 @@ package com.mindolph.fx.view;
 import com.mindolph.base.FontIconManager;
 import com.mindolph.core.model.NodeData;
 import com.mindolph.fx.event.DragFileEventHandler;
+import com.mindolph.mfx.dialog.DialogFactory;
+import com.mindolph.mfx.dialog.DialogFactory.MultiConfirmation;
 import com.mindolph.mfx.util.FontUtils;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TreeCell;
@@ -15,11 +17,14 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -62,8 +67,15 @@ public class WorkspaceViewCell extends TreeCell<NodeData> {
         });
         this.setOnDragOver(dragEvent -> {
             if (dragEvent.getDragboard().hasString()) {
-                if (getTreeItemData() != null && (getTreeItemData().isFolder() || getTreeItemData().isWorkspace())) {
+                if (this.isDroppable()) {
                     dragEvent.acceptTransferModes(TransferMode.MOVE);
+                    log.trace("ok to drop");
+                }
+            }
+            else if (dragEvent.getDragboard().hasFiles()) {
+                // this is for external files (since the hasString() method has already checked with internal files.)
+                if (this.isDroppable()) {
+                    dragEvent.acceptTransferModes(TransferMode.COPY);
                     log.trace("ok to drop");
                 }
             }
@@ -74,7 +86,7 @@ public class WorkspaceViewCell extends TreeCell<NodeData> {
         });
         this.setOnDragEntered(dragEvent -> {
             log.trace("drag entered: " + (this.getTreeItem() == null ? EMPTY : this.getTreeItem().getValue()));
-            if (getTreeItemData() != null && (getTreeItemData().isFolder() || getTreeItemData().isWorkspace())) {
+            if (this.isDroppable()) {
                 origBackground = this.getBackground();
                 this.setBorder(new Border(new BorderStroke(Color.DARKBLUE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
             }
@@ -83,19 +95,19 @@ public class WorkspaceViewCell extends TreeCell<NodeData> {
                 public void run() {
                     if (!getTreeItem().isExpanded()) getTreeItem().setExpanded(true);
                 }
-            }, 1000);
+            }, 1500);
             dragEvent.consume();
         });
         this.setOnDragExited(dragEvent -> {
             log.trace("drag exited");
-            if (getTreeItemData() != null && (getTreeItemData().isFolder() || getTreeItemData().isWorkspace())) {
+            if (this.isDroppable()) {
                 this.setBorder(null);
             }
             dragEvent.consume();
         });
         this.setOnDragDropped(dragEvent -> {
             if (dragEvent.getDragboard().hasString()) {
-                if (getTreeItemData() != null && (getTreeItemData().isFolder() || getTreeItemData().isWorkspace())) {
+                if (this.isDroppable()) {
                     if (draggingNodeDatas != null && !draggingNodeDatas.isEmpty()) {
                         log.info(String.format("Drag %s and drop to %s", dragEvent.getDragboard().getString(), getTreeItemData().getFile().toString()));
                         dragFileEventHandler.onFilesDragged(draggingNodeDatas, getTreeItemData());
@@ -108,11 +120,67 @@ public class WorkspaceViewCell extends TreeCell<NodeData> {
                     log.debug("Dropped on a non folder node");
                 }
             }
+            else if (dragEvent.getDragboard().hasFiles()) {
+                getTreeView().requestFocus();
+                if (this.isDroppable()) {
+                    List<File> files = dragEvent.getDragboard().getFiles();
+                    NodeData nodeData = getTreeItemData();
+                    List<TreeItem<NodeData>> newItems = new ArrayList<>();
+                    boolean yesToAll = false;
+                    boolean noToAll = false;
+                    MultiConfirmation multiConfirmation = null;
+                    for (File file : files) {
+                        File destFile = new File(nodeData.getFile(), file.getName());
+                        log.debug("try to copy file {} to {}", file, destFile);
+                        if (destFile.exists()) {
+                            if (!yesToAll && !noToAll) {
+                                multiConfirmation = DialogFactory.multiConfirmDialog("Confirm Overwrite",
+                                        "File %s already exists, are you sure to overwrite it?".formatted(file.getName()));
+                                if (multiConfirmation == null) {
+                                    break;// quit coping files
+                                }
+                            }
+                            if (yesToAll || MultiConfirmation.isPositive(multiConfirmation)) {
+                                try {
+                                    FileUtils.copyFile(file, destFile);
+                                } catch (IOException e) {
+                                    DialogFactory.errDialog("Failed to copy file " + file.getName() + " to " + destFile.getAbsolutePath());
+                                }
+                                log.debug("copy file {} with overwrite done", file);
+                            }
+                            yesToAll = multiConfirmation == MultiConfirmation.YES_TO_ALL;
+                            noToAll = multiConfirmation == MultiConfirmation.NO_TO_ALL;
+                        }
+                        else {
+                            try {
+                                FileUtils.copyFile(file, destFile);
+                            } catch (IOException e) {
+                                DialogFactory.errDialog("Failed to copy file " + file.getName() + " to " + destFile.getAbsolutePath());
+                            }
+                            log.debug("copy file {} done", file);
+                            NodeData destNodeData = new NodeData(destFile);
+                            destNodeData.setWorkspaceData(nodeData.getWorkspaceData());
+                            newItems.add(new TreeItem<>(destNodeData));
+                        }
+                    }
+                    getTreeItem().getChildren().addAll(newItems);
+                    getTreeView().getSelectionModel().clearSelection();
+                    newItems.forEach(ti -> getTreeView().getSelectionModel().select(ti));
+                    getTreeView().refresh();
+                    getTreeView().requestFocus();
+                }
+            }
             else {
                 log.warn("no string");
             }
             dragEvent.consume();
         });
+    }
+
+    private boolean isDroppable() {
+        NodeData nodeData = getTreeItemData();
+        if (nodeData == null) return false;
+        return (nodeData.isFolder() || nodeData.isWorkspace());
     }
 
     private NodeData getTreeItemData() {
