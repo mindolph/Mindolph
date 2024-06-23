769fc3af1c4ee92e8d2381049611de7001743926
mindolph <mindolph.com@gmail.com>
2024-06-23 17:12:10 +0800

    fix: exception when locating root topic of mind map from search result; fix: When locating a mind map topic from the search results, the selected topics are not cleared;

--- code/mindolph-mindmap/src/main/java/com/mindolph/mindmap/MindMapEditor.java
+++ code/mindolph-mindmap/src/main/java/com/mindolph/mindmap/MindMapEditor.java
@@ -152,11 +152,13 @@ public class MindMapEditor extends BaseEditor {
             firstInTree = this.getMindMapModel().findFirstInTree(t -> {
                 log.debug("  %s".formatted(t.getText()));
                 // TODO within extra contents if how to display them in the result has been resolved.
-                return ma.getText().equals(t.getText()) && ma.getParentText().equals(t.getParent().getText());
+                return ma.getText().equals(t.getText())
+                        && (t.getParent() == null || ma.getParentText().equals(t.getParent().getText()));
             });
         }
         if (firstInTree.isPresent()) {
-            log.debug("Find topic node: " + firstInTree.get().getText());
+            log.debug("Find topic node: %s".formatted(firstInTree.get().getText()));
+            this.mindMapView.getSelection().clear();
             this.mindMapView.selectAndUpdate(firstInTree.get(), false);
         }
         else {
