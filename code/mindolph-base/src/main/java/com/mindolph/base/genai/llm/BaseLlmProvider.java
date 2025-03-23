f45e707bb006ffa56cb3d33e5721fa9f718aa46f
iorranlira <iorranlira@gmail.com>
2025-03-22 22:23:50 -0300

    Adicionar um caso default no switch case.

--- code/mindolph-base/src/main/java/com/mindolph/base/genai/llm/BaseLlmProvider.java
+++ code/mindolph-base/src/main/java/com/mindolph/base/genai/llm/BaseLlmProvider.java
@@ -1,17 +1,17 @@
 package com.mindolph.base.genai.llm;
 
-import com.mindolph.base.constant.PrefConstants;
-import com.mindolph.base.genai.GenAiEvents.Input;
-import com.mindolph.core.constant.GenAiConstants;
-import com.mindolph.mfx.preference.FxPreferences;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.mindolph.base.constant.PrefConstants;
 import static com.mindolph.base.constant.PrefConstants.GEN_AI_TIMEOUT;
+import com.mindolph.base.genai.GenAiEvents.Input;
+import com.mindolph.core.constant.GenAiConstants;
+import com.mindolph.mfx.preference.FxPreferences;
 
 /**
  * @author mindolph.com@gmail.com
@@ -28,7 +28,7 @@ public abstract class BaseLlmProvider implements LlmProvider {
     protected final String apiKey;
     protected final String aiModel;
     protected final boolean useProxy;
-    protected final String TEMPLATE = """
+    protected final static String TEMPLATE = """
             {{input}}.
             {{format}}.
             {{length}}.
